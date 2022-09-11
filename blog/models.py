df54c48a3aa8edcfaafddb811481c792a0571d6f
frankiesanjana <francesca.sanjana@gmail.com>
2022-09-11 15:16:02 +0000

    Add walkthrough videos to Admin Pt 1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# defines the class for a blog post (cf a table in the database)
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+    # the minus sign in the line below means to use descending order ie most recent first
+        ordering = ['-created_on']
+
+    # Then we're going to use this string method  here, it's good practice to put that into your projects.
+    # The Django documentation says it's a  magic method that returns a string representation  
+    # of an object and it says you should define  it because the default isn't helpful at all.
+
+    def __str__(self):
+        return self.title
+
+    # Finally, we have a helper method to return the total number of likes on a post.
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(blank=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
